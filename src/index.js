153a22c4c71c32791c8ca3a4dec84d3c54990cd6
Joe Dietrich <joe@joedietrich.net>
2021-07-16 21:50:02 -0400

    Done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+  inputForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const input = document.querySelector('input#searchByID');
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then(response => response.json())
+      .then(({ title, summary }) => {
+        console.log(title, summary)
+        const titleArea = document.querySelector('section#movieDetails h4');
+        const summaryArea = document.querySelector('section#movieDetails p');
+        titleArea.innerText = title;
+        summaryArea.innerText = summary;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);
